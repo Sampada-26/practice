89a7cf31267324498703bebe17e9f34f3d94d8b1
Sampada-26 <kaginkarsampada@gmail.com>
2026-04-02 00:28:50 -0400

    function closure

---
+++ followers_count/followers_count.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+function checkCountValue(){
+    if (count==10){
+        alert("Your insta post gained 10 followers");
+    }
+    else if (count==20){
+        alert("Your insta post gained 20 followers");
+    }
+}
+function increaseCount() {
+  count++; // Increment the count by 1
+  displayCount(); // Display the count
+  checkCountValue(); // Check count value and display messages
+}
